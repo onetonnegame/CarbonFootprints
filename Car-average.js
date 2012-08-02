3bafb98b177883cae705ba27dfba48dcc6e8a76e
onetonnegame <info@onetonnegame.com>
2012-08-02 16:52:05 +0200

    Update Car-average.js

--- Car-average.js
+++ Car-average.js
@@ -1,7 +1,9 @@
 {
-'name': 'Car ownership',
+'name': 'e.g. Car ownership',
 'footprint-average usage': 100,
 'footprint-low usage': 50,
 'footprint-high usage': 200,
-'category' : 'transport'
+'category' : 'transport',
+'source' : 'complete guess',
+'notes' : 'this is an example.'
 }
